c52c0a548e45d59832b5fcd48bce82a698381c5c
Span1shUk <31575380+Span1shUk@users.noreply.github.com>
2021-08-17 20:20:47 +0100

    Create Q20Tuple.py

---
+++ Q20Tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+##################################################
+## Hello World
+##################################################
+## Version: 1.0
+## Maintainer: Jose
+## Email: jose_vila@icloud.com
+##################################################
+tuple = (1, 2 , 3, 4)
+tuple.append( (5, 6, 7) )
+print (len(my_tuple))
